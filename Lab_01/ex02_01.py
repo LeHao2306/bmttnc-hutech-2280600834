d3e8e27f503d29f38bb9508d99b35b1456662fbb
Le Nhut Hao <lenhuthao23062004@gmail.com>
2025-02-25 11:28:57 +0700

    [add] lab01 ex01

---
+++ Lab_01/ex02_01.py
@@ -0,0 +1,5 @@
+#Nhập tên và tuổi từ người dùng
+ten = input("Nhap ten cua ban: ")
+tuoi = input("Nhap tuoi cua ban: ")
+#In thông điệp chào mừng và thông tin người nhập
+print("Chào mừng,", ten, "! Bạn", tuoi, "tuổi.")
